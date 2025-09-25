973286ecbb4beeb419089fdc1324a3dd2dba50ae
Ozan Şen <ozanokan1903@gmail.com>
2025-09-25 13:33:11 +0300

    basic_math.py file created.

---
+++ basic_math.py
@@ -0,0 +1,16 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    pass
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError('Division By Zero Error')
+    return x / y
+
+
+
